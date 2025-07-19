ef79c25fed2cb4cf15179a3ca998611ad3c3f5c5
Shaqayeq Temori <edristemory33@gmail.com>
2025-07-19 13:30:22 +0430

    new

--- database/migrations/2025_07_09_092656_create_products_table.php
+++ database/migrations/2025_07_09_092656_create_products_table.php
@@ -14,20 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // barnd key
-            $table->foreignId('brand_id');
-            $table->foreign('brand_id')->references('id')->on('brands')->onDelet('cascade');
-            // category key
-            $table->foreignId('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelet('cascade');
-            $table->string('slug')->unique();
             $table->string('primary_image');
             $table->text('description');
-            $table->integer('status')->default(1);
-            $table->boolean('is_active')->default(1);
-            $table->unsignedInteger('delivery_amount_per_product')->nullable();
-            $table->unsignedInteger('delivery_amount')->default(0);
-        
+            $table->unsignedBigInteger('brand_id')->default(1);
             $table->timestamps();
         });
     }
